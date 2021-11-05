bbc5cf00d47abdbd024cbbf3e58cd4d7bc7ceea6
Jarweb01 <jarvisweb@mail.ru>
2021-11-06 02:11:31 +0300

    header start

--- home/src/components/Header.vue
+++ home/src/components/Header.vue
@@ -1,7 +1,35 @@
 <template>
-  <div class="hello">
-    <h1>Hello</h1>
-  </div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button> -->
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item" v-for="(menu, index) in headerMenuList" :key="index">
+            <router-link class="nav-link active" aria-current="page" :to="menu.path">
+              {{ menu.name }}
+            </router-link>
+          </li>
+        </ul>
+        <div>
+          <a href="#">
+            <img src="../assets/images/icons/facebook.png" alt="">
+          </a>
+          <a href="#">
+            <img src="../assets/images/icons/p.png" alt="">
+          </a>
+          <a href="#">
+            <img src="../assets/images/icons/inst.png" alt="">
+          </a>
+          <a href="#">
+            <img src="../assets/images/icons/g+.png" alt="">
+          </a>
+        </div>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -9,7 +37,17 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  }, data () {
+    return {
+      headerMenuList: [
+        {name: 'Home', path: '/'},
+        {name: 'About Us', path: '/about'},
+        {name: 'Property', path: '/property'},
+        {name: 'Our Blog', path: '/blog'},
+        {name: 'Contacts', path: '/contacts'},
+      ]
+    }
+  },
 }
 </script>
 
